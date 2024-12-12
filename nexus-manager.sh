d30605f06e0c64c04633ce5dcbd5914efe0d4b02
linflg <51857593+linflg@users.noreply.github.com>
2024-12-12 23:05:02 +0800

    Update nexus-manager.sh

--- nexus-manager.sh
+++ nexus-manager.sh
@@ -115,7 +115,7 @@ download_prover() {
         elif [ "$OS" = "Linux" ]; then
             if [ "$ARCH" = "x86_64" ]; then
                 echo -e "${YELLOW}下载 Linux AMD64 架构 Prover...${NC}"
-                curl -L "https://github.com/qzz0518/nexus-run/releases/download/v0.4.2/prover-amd64" -o "$prover_path"
+                curl -L "https://github.com/linflg/nexus-run/releases/download/v1.0.0/prover" -o "$prover_path"
             else
                 echo -e "${RED}不支持的 Linux 架构: $ARCH${NC}"
                 exit 1
